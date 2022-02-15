17b999ceb0e379929c2e81139fb7cab0c2e237ae
Sai-Myo-Myat <saimyomyat511@gmail.com>
2022-02-15 15:52:46 +0630

    update2

---
+++ src/images.js
@@ -0,0 +1,8 @@
+import image1 from "../assests/image1";
+import image2 from "../assests/image2";
+import image3 from "../assests/image3";
+import image4 from "../assests/image4";
+import image5 from "../assests/image5";
+import image6 from "../assests/image6";
+
+export default [image1, image2, image3, image4, image5, image6];
