104066f8f9c8e0d95b05a577f06209ed96420a30
Sai-Myo-Myat <saimyomyat511@gmail.com>
2022-02-15 22:22:56 +0630

    having error

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import "./App.css";
 import { motion } from "framer-motion";
+//importing component
+import images from "./images";
+import { useState, useEffect, useRef } from "react";
 
 function App() {
+  const [width, setWidth] = useState(0);
+  const carousal = useRef();
+  useEffect(() => {
+    console.log(carousal.current.scrollWidth);
+    console.log(carousal.current.offsetLeft);
+    setWidth(carousal.current.scrollWidth - carousal.current.offsetWidth);
+  }, []);
+
   return (
     <div className="App">
-      <motion.div className="carousal">
-        <motion.div className="inner-carousal">d</motion.div>
+      <motion.div ref={carousal} className="carousal">
+        <motion.div
+          drag="x"
+          dragConstraints={{ right: 0, left: -width }}
+          className="inner-carousal"
+        >
+          {images.map((image) => (
+            <motion.div className="item" key={image}>
+              <img src={image} alt="" />{" "}
+            </motion.div>
+          ))}
+        </motion.div>
       </motion.div>
     </div>
   );
--- src/images.js
+++ src/images.js
@@ -1,8 +1,8 @@
-import image1 from "../assests/image1";
-import image2 from "../assests/image2";
-import image3 from "../assests/image3";
-import image4 from "../assests/image4";
-import image5 from "../assests/image5";
-import image6 from "../assests/image6";
+import image1 from "./assests/image1.jpeg";
+import image2 from "./assests/image2.jpeg";
+import image3 from "./assests/image3.jpeg";
+import image4 from "./assests/image4.jpeg";
+import image5 from "./assests/image5.jpeg";
+import image6 from "./assests/image6.jpeg";
 
 export default [image1, image2, image3, image4, image5, image6];
